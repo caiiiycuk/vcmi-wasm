f7e9c08a9f7a7c426fec96b2c2d8a4ed85f282d1
Alexander Guryanov <caiiiycuk@gmail.com>
2025-01-30 12:11:23 +0300

    update publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -12,7 +12,7 @@ mkdir ../../vcmi-html5-launcher/public/vcmi/
 cp html5/vcmi* ../../vcmi-html5-launcher/public/vcmi/
 
 #copy Mp3
-cp -r ../data/Mp3 ../../vcmi-html5-launcher/public/vcmi/Mp3/
+cp -r ../scripts/Mp3Async/* ../../vcmi-html5-launcher/public/vcmi/
 
 #copy additional data
 cp -rf *.data* ../../vcmi-html5-launcher/public/vcmi/
